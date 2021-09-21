f0fb06dd2b1bf0e80331bdfafd93d0e60cfa8275
frosty03 <jatin.chourasia@supista.com>
2021-09-21 23:49:45 +0530

    initial

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,11 @@ import { ExampleComponent } from 'component-plugin-library-template'
 import 'component-plugin-library-template/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div className=''>
+      <ExampleComponent text='Create React Library Example 😄' />
+    </div>
+  )
 }
 
 export default App
