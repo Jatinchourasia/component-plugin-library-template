f75edf8b8b64bcf19a4a2ce204e9a95c385dad39
frosty03 <jatin.chourasia@supista.com>
2021-09-22 00:09:03 +0530

    Updates

---
+++ precache-manifest.7feb0f4d3938ed8ad9c5bb9315ef70ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20f1d417582d14e34f9104dedcd0cb6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7e5e2e0d9b247857aea",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "43f35f3211fb1961dddf",
+    "url": "./static/js/2.8ba60363.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.8ba60363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e5e2e0d9b247857aea",
+    "url": "./static/js/main.52428264.chunk.js"
+  },
+  {
+    "revision": "8fe6143730de4fcda463",
+    "url": "./static/js/runtime-main.3c21de8d.js"
+  }
+]);
